6520dea3b54357a7be5844bb109b80fc3b46584e
andriesthobs <72280067+andriesthobs@users.noreply.github.com>
2024-04-25 14:02:13 +0200

    Update Incident_Breakdown.py

--- pages/Incident_Breakdown.py
+++ pages/Incident_Breakdown.py
@@ -1,56 +1,53 @@
-import plotly.validators.surface
-from plotly.validators import surface
+import plotly.graph_objects as go
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
-import numpy as np
 
-st.set_page_config(page_title="Incident Breakdown Dashboard",page_icon=":bar_chart",layout="wide")
+st.set_page_config(page_title="Incident Breakdown Dashboard", page_icon=":bar_chart", layout="wide")
 
 st.title("Incident Breakdown")
 st.markdown("_v1.0.0_")
 
 with st.sidebar:
     st.header("Incident Sheet")
-    upload_file=st.file_uploader("Choose file")
+    upload_file = st.file_uploader("Choose file")
 
 if upload_file is None:
-    st.info("upload file through config",icon="ℹ️")
+    st.info("Please upload a file through the configuration", icon="ℹ️")
     st.stop()
 
 @st.cache_data
 def load_data(path: str):
-    df=pd.read_excel(path)
+    df = pd.read_excel(path)
     return df
 
-df= load_data(upload_file)
+df = load_data(upload_file)
 
 df['Opened'] = pd.to_datetime(df['Opened'], errors='coerce').dt.strftime('%Y-%m-%d')
-#sidebar
+
 st.sidebar.header("Please filter here :")
 
-Manager=st.sidebar.multiselect(
-    "Select the S Manager",
-    options=df["Manager"].unique(),  
-)
-
-Company= st.sidebar.multiselect(
+Company = st.sidebar.multiselect(
     "Select the Client",
     options=df["Company"].unique(),
 )
 
-if not Manager:
-    st.info("Please select  S Manager to get started.")
+Manager = st.sidebar.multiselect(
+    "Select the S Manager",
+    options=df["Manager"].unique(),  
+)
+
+if not Manager or not Company:
+    st.info("Please select both a Client/Company and an S Manager to get started.")
     st.stop()
 
-df_selection= df.query("Manager==@Manager and Company==@Company")
+df_selection = df.query("Manager == @Manager and Company == @Company")
 
-totalNoOfIncident=int(df["Company"].count())
-totNoSM=int(df_selection["Company"].count())
-incidentPerSM=(totNoSM/totalNoOfIncident)*100
+totalNoOfIncident = int(df["Company"].count())
+totNoSM = int(df_selection["Company"].count())
+incidentPerSM = (totNoSM / totalNoOfIncident) * 100
 
-incC1,incC2,incPer= st.columns(3)
+incC1, incC2, incPer = st.columns(3)
 
 with incC1:
     st.subheader("Total Incidents per SMM")
@@ -61,36 +58,35 @@ with incC2:
     st.subheader(totNoSM)
 
 with incPer:
-    st.subheader("Percentage per SM Selection")
-    st.subheader( "%.2f" % incidentPerSM +" % ")
+    st.subheader("Percentage per SM")
+    if totNoSM == 0:
+        st.subheader("You cannot Divide by Zero")
+    else:
+        st.subheader("%.2f" % incidentPerSM + " % ")
 
 st.markdown("-------------------------------")
 
-pivot_table= df_selection.pivot_table(index='Opened',columns="Company",aggfunc='size',fill_value=0)
-data=[]
+pivot_table = df_selection.pivot_table(index='Opened', columns="Company", aggfunc='size', fill_value=0)
+data = []
 for column in pivot_table.columns:
     trace = go.Scatter(x=pivot_table.index, y=pivot_table[column], mode='lines+markers', name=column)
     data.append(trace)
 
-layout=go.Layout(title="Incidents Count per day",xaxis=dict(title='Opened'),yaxis=dict(title='Count'))
-fig2=go.Figure(data=data,layout=layout)
-#st.plotly_chart(fig2)
+layout = go.Layout(title="Incidents Count per day", xaxis=dict(title='Opened'), yaxis=dict(title='Count'))
+fig2 = go.Figure(data=data, layout=layout)
+
 ##############################Incident Count#####################################
-df_grouped=df_selection.groupby(['Company','Resolution code']).size().reset_index(name='Count')
-df_pivot=df_grouped.pivot(index='Company',columns='Resolution code',values='Count').fillna(0)
+df_grouped = df_selection.groupby(['Company', 'Resolution code']).size().reset_index(name='Count')
+df_pivot = df_grouped.pivot(index='Company', columns='Resolution code', values='Count').fillna(0)
 
-fig=px.bar(df_pivot,x=df_pivot.index,y=df_pivot.columns,title="Incidents Count Per Customer",labels={'value':'Count','index':'Company',},barmode='stack')
-
-#st.plotly_chart(fig)
+fig = px.bar(df_pivot, x=df_pivot.index, y=df_pivot.columns, title="Incidents Count Per Customer", barmode='stack')
 
 ############################Priority####################################
 
-df_grouped=df_selection.groupby(['Company','Priority']).size().reset_index(name='Count')
-df_pivot=df_grouped.pivot(index='Company',columns='Priority',values='Count').fillna(0)
+df_grouped = df_selection.groupby(['Company', 'Priority']).size().reset_index(name='Count')
+df_pivot = df_grouped.pivot(index='Company', columns='Priority', values='Count').fillna(0)
 
-fig3=px.bar(df_pivot,x=df_pivot.index,y=df_pivot.columns,title="Priority  Count Per Customer",labels={'value':'Count','index':'Company',},barmode='stack')
-
-#st.plotly_chart(fig3)
+fig3 = px.bar(df_pivot, x=df_pivot.index, y=df_pivot.columns, title="Priority  Count Per Customer", barmode='stack')
 
 ##############################Company Incident Percentage##########################################
 df_grouped = df_selection.groupby('Company').size().reset_index(name='Count')
@@ -103,11 +99,9 @@ fig_pie = px.pie(df_grouped, names='Company', values='Count',
                  hole=0.4)
 fig_pie.update_traces(textposition='inside', textinfo='label+percent')
 
-#st.plotly_chart(fig_pie)
-
-incCountPerDay,incCountPerCust= st.columns(2)
+incCountPerDay, incCountPerCust = st.columns(2)
 st.markdown("---------------------------------------------------------")
-PrioPerCust,percPerCust=st.columns(2)
+PrioPerCust, percPerCust = st.columns(2)
 st.markdown("---------------------------------------------------------")
 
 with incCountPerDay:
@@ -116,10 +110,8 @@ with incCountPerDay:
 with incCountPerCust:
     st.plotly_chart(fig)
 
-
 with PrioPerCust:
     st.plotly_chart(fig3)
 
 with percPerCust:
     st.plotly_chart(fig_pie)
-
